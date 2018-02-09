e64babda32a250f63513c3be4deb3fc9b9fff2ba
monofuel <monofuel@japura.net>
2018-02-09 00:03:14 -0800

    typing

--- frontend/api.ts
+++ frontend/api.ts
@@ -7,12 +7,12 @@ interface AuthLoginOpts {
 interface AuthLoginResp {
 	oauth_url: string;
 }
-export async function userAuthLoginGet(params?: AuthLoginOpts) {
+export async function userAuthLoginGet(params?: AuthLoginOpts): Promise<string> {
 	const resp = await axios.get<AuthLoginResp>('/api/user/auth/login', { params });
 	return resp.data.oauth_url;
 }
 
-export async function userInfoGet() {
+export async function userInfoGet(): Promise<User> {
 	const resp = await axios.get<User>('/api/user/info', );
 	return resp.data;
 }
