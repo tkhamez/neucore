4263a4c5600b9ae1922561dd9ef6eb3fddf9368e
Tian Khamez <tkhamez@gmail.com>
2018-04-16 23:31:04 +0200

    removed example app

---
+++ backend/src/classes/Brave/Core/Migrations/Version20180416212822.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types = 1);
+namespace Brave\Core\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20180416212822 extends AbstractMigration
+{
+
+    public function up(Schema $schema)
+    {
+        $this->addSql('DELETE FROM apps WHERE id = :id', array('id' => 1));
+    }
+
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+    }
+}
