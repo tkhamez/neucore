95de1bf4f7a7b0d266aaae53f3b2e22e03f3b52f
Tian Khamez <tkhamez@gmail.com>
2018-05-11 19:12:27 +0000

    fixed missing semicolon and moved code in a function

--- web/index.src.js
+++ web/index.src.js
@@ -1,150 +1,152 @@
-'use strict'
+(function () {
+	'use strict';
 
-var SwaggerBrvneucoreJs = require('bravecore-swagger-js-client'); /* jshint ignore: line */
-var defaultClient = SwaggerBrvneucoreJs.ApiClient.instance;
-defaultClient.basePath = location.protocol + "//" + location.hostname + ':' + location.port + '/api';
+	var SwaggerBrvneucoreJs = require('bravecore-swagger-js-client'); /* jshint ignore: line */
+	var defaultClient = SwaggerBrvneucoreJs.ApiClient.instance;
+	defaultClient.basePath = location.protocol + "//" + location.hostname + ':' + location.port + '/api';
 
-var bravecore = new window.Vue({
-	el : '#app',
+	var bravecore = new window.Vue({
+		el : '#app',
 
-	data : {
-		loginUrl : null,
-		loginAltUrl : null,
-		loginErrorMessage: '',
-		authChar : null,
-		player : {},
-		loadingCount: 0
-	},
+		data : {
+			loginUrl : null,
+			loginAltUrl : null,
+			loginErrorMessage: '',
+			authChar : null,
+			player : {},
+			loadingCount: 0
+		},
 
-	mounted : function() {
-		this.getLoginUrl();
-		this.getLoginAltUrl();
-		this.getCharacter();
-		this.getPlayer();
-		if (location.hostname === 'brvneucore.herokuapp.com') {
-			window.$('#preview').show();
-		}
-		if (location.hash === '#login' || location.hash === '#login-alt') {
-			this.getAuthResult();
-			location.hash = '';
-		}
-	},
-
-	methods : {
-
-		loading: function (status) {
-			if (status) {
-				this.loadingCount ++;
-			} else {
-				this.loadingCount --;
+		mounted : function() {
+			this.getLoginUrl();
+			this.getLoginAltUrl();
+			this.getCharacter();
+			this.getPlayer();
+			if (location.hostname === 'brvneucore.herokuapp.com') {
+				window.$('#preview').show();
 			}
-			if (this.loadingCount > 0) {
-				window.$("#loader").fadeIn();
-			} else {
-				window.$("#loader").fadeOut();
+			if (location.hash === '#login' || location.hash === '#login-alt') {
+				this.getAuthResult();
+				location.hash = '';
 			}
 		},
 
-		getLoginUrl : function() {
-			this.loading(true);
-			new SwaggerBrvneucoreJs.AuthApi().loginUrl({
-				redirect: '/#login'
-			}, function(error, data) {
-				bravecore.loading(false);
-				if (error) {
-					window.console.error(error);
-					return;
-				}
-				bravecore.loginUrl = data;
-			});
-		},
+		methods : {
 
-		getLoginAltUrl : function() {
-			this.loading(true);
-			new SwaggerBrvneucoreJs.AuthApi().loginAltUrl({
-				redirect: '/#login-alt'
-			}, function(error, data) {
-				bravecore.loading(false);
-				if (error) { // 403 usually
-					return;
+			loading: function (status) {
+				if (status) {
+					this.loadingCount ++;
+				} else {
+					this.loadingCount --;
 				}
-				bravecore.loginAltUrl = data;
-			});
-		},
+				if (this.loadingCount > 0) {
+					window.$("#loader").fadeIn();
+				} else {
+					window.$("#loader").fadeOut();
+				}
+			},
 
-		getAuthResult: function() {
-			this.loading(true);
-			new SwaggerBrvneucoreJs.AuthApi().result(function(error, data) {
-				bravecore.loading(false);
-				if (error) {
-					window.console.error(error);
-					return;
-				}
-				if (! data.success) {
-					bravecore.loginErrorMessage = data.message;
-					window.$("#msg-login-error").show();
-				}
-			});
-		},
-
-		getCharacter : function() {
-			this.loading(true);
-			new SwaggerBrvneucoreJs.CharacterApi().show(function(error, data) {
-				bravecore.loading(false);
-				if (error) { // 403 usually
-					bravecore.authChar = null;
-					return;
-				}
-				bravecore.authChar = data;
-			});
-		},
-
-		getPlayer : function() {
-			this.loading(true);
-			new SwaggerBrvneucoreJs.PlayerApi().show(function(error, data) {
-				bravecore.loading(false);
-				if (error) { // 403 usually
-					return;
-				}
-				bravecore.player = data;
-			});
-		},
-
-		logout : function() {
-			this.loading(true);
-			new SwaggerBrvneucoreJs.AuthApi().logout(function(error) {
-				bravecore.loading(false);
-				if (error) { // 403 usually
-					return;
-				}
-				bravecore.getCharacter();
-				bravecore.getLoginUrl();
-			});
-		},
-
-		makeMain : function(characterId) {
-			this.loading(true);
-			new SwaggerBrvneucoreJs.PlayerApi().setMain(characterId, function(error) {
-				bravecore.loading(false);
-				if (error) { // 403 usually
-					return;
-				}
-				bravecore.getPlayer();
-			});
-		},
-
-		update : function(characterId) {
-			this.loading(true);
-			new SwaggerBrvneucoreJs.CharacterApi().update(characterId, function(error) {
-				bravecore.loading(false);
-				if (error) { // 403 usually
-					return;
-				}
-				bravecore.getPlayer();
-				window.$("#msg-update-done").fadeIn(500, function() {
-					window.$(this).delay(1500).slideUp(500);
+			getLoginUrl : function() {
+				this.loading(true);
+				new SwaggerBrvneucoreJs.AuthApi().loginUrl({
+					redirect: '/#login'
+				}, function(error, data) {
+					bravecore.loading(false);
+					if (error) {
+						window.console.error(error);
+						return;
+					}
+					bravecore.loginUrl = data;
 				});
-			});
+			},
+
+			getLoginAltUrl : function() {
+				this.loading(true);
+				new SwaggerBrvneucoreJs.AuthApi().loginAltUrl({
+					redirect: '/#login-alt'
+				}, function(error, data) {
+					bravecore.loading(false);
+					if (error) { // 403 usually
+						return;
+					}
+					bravecore.loginAltUrl = data;
+				});
+			},
+
+			getAuthResult: function() {
+				this.loading(true);
+				new SwaggerBrvneucoreJs.AuthApi().result(function(error, data) {
+					bravecore.loading(false);
+					if (error) {
+						window.console.error(error);
+						return;
+					}
+					if (! data.success) {
+						bravecore.loginErrorMessage = data.message;
+						window.$("#msg-login-error").show();
+					}
+				});
+			},
+
+			getCharacter : function() {
+				this.loading(true);
+				new SwaggerBrvneucoreJs.CharacterApi().show(function(error, data) {
+					bravecore.loading(false);
+					if (error) { // 403 usually
+						bravecore.authChar = null;
+						return;
+					}
+					bravecore.authChar = data;
+				});
+			},
+
+			getPlayer : function() {
+				this.loading(true);
+				new SwaggerBrvneucoreJs.PlayerApi().show(function(error, data) {
+					bravecore.loading(false);
+					if (error) { // 403 usually
+						return;
+					}
+					bravecore.player = data;
+				});
+			},
+
+			logout : function() {
+				this.loading(true);
+				new SwaggerBrvneucoreJs.AuthApi().logout(function(error) {
+					bravecore.loading(false);
+					if (error) { // 403 usually
+						return;
+					}
+					bravecore.getCharacter();
+					bravecore.getLoginUrl();
+				});
+			},
+
+			makeMain : function(characterId) {
+				this.loading(true);
+				new SwaggerBrvneucoreJs.PlayerApi().setMain(characterId, function(error) {
+					bravecore.loading(false);
+					if (error) { // 403 usually
+						return;
+					}
+					bravecore.getPlayer();
+				});
+			},
+
+			update : function(characterId) {
+				this.loading(true);
+				new SwaggerBrvneucoreJs.CharacterApi().update(characterId, function(error) {
+					bravecore.loading(false);
+					if (error) { // 403 usually
+						return;
+					}
+					bravecore.getPlayer();
+					window.$("#msg-update-done").fadeIn(500, function() {
+						window.$(this).delay(1500).slideUp(500);
+					});
+				});
+			}
 		}
-	}
-});
+	});
+})();
