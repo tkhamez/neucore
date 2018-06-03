8f87adb0340f35e8dfb1920fe40d38c98ceee77b
Tian Khamez <tkhamez@gmail.com>
2018-06-03 19:14:08 +0000

    webpack config

--- frontend2/webpack.config.js
+++ frontend2/webpack.config.js
@@ -1,38 +1,62 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const webpack = require('webpack');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-module.exports = {
-    entry: {
-        'vendor': './src/vendor.js',
-        'app': './src/index.js'
-    },
-    output: {
-        path: path.resolve(__dirname, '../web'),
-        filename: '[name].js'
-    },
-    module: {
-        rules: [
-            {
+module.exports = (env, argv) => {
+    const devMode = argv.mode !== 'production';
+    return {
+        entry: {
+            'vendor': './src/vendor.js',
+            'app': './src/index.js'
+        },
+        output: {
+            path: path.resolve(__dirname, '../web/dist'),
+            filename: devMode ? '[name].js' : '[name].[chunkhash].js'
+        },
+        module: {
+            rules: [{
                 test: /\.(css|scss)$/,
                 use: [
-                    'style-loader', 'css-loader', 'sass-loader'
+                    devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader'
                 ]
             }, {
                 // for open-iconic fonts
                 test: /\.(woff|woff2|eot|ttf|otf|svg)$/,
-                use: [
-                    {
-                        loader: 'file-loader',
-                        options: {
-                            name: 'fonts/[name].[ext]'
-                        }
-                    }
-                ]
+                use: [{
+                    loader: 'file-loader',
+                    options: { name: '../fonts/[name].[ext]' }
+                }]
             }, {
-                // AMD define module paths fails in Webpack
-                // https://github.com/swagger-api/swagger-codegen/issues/3466
+                // Swagger client AMD define fails, https://github.com/swagger-api/swagger-codegen/issues/3466
                 test: /brvneucore-js-client\/.*\.js$/,
                 use: 'imports-loader?define=>false'
-            }
-        ]
+            }]
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: 'src/index.html',
+                filename: '../index.html'
+            }),
+            new CleanWebpackPlugin(['dist'], {
+                root: path.resolve(__dirname, '../web')
+            }),
+            new webpack.DefinePlugin({
+                'process.env.NODE_ENV': JSON.stringify(devMode ? 'development' : 'production')
+            }),
+            new MiniCssExtractPlugin({
+                filename: "[name].[chunkhash].css",
+                //chunkFilename: "[name].[id].[chunkhash].css"
+            })
+        ],
+        optimization: {
+            runtimeChunk: true,
+            splitChunks: { chunks: 'all' }
+        },
+        devtool: devMode ? 'inline-source-map' : 'source-map',
+        performance: { hints: false }
     }
 };
