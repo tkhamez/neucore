f6a00f31d5e3d316f02a02c95124215c8314a22f
Tian Khamez <tkhamez@gmail.com>
2020-05-02 11:22:35 +0200

    Adjusted order of jobs

--- backend/bin/run-jobs.sh
+++ backend/bin/run-jobs.sh
@@ -2,12 +2,12 @@
 
 DIR=$(dirname "$(realpath "$0")")
 
-${DIR}/console update-chars --log --hide-details
-${DIR}/console update-corporations --log --hide-details
-${DIR}/console update-player-groups --log --hide-details
-${DIR}/console check-tokens --log --hide-details
-${DIR}/console update-member-tracking --log
-${DIR}/console send-invalid-token-mail --log
-${DIR}/console send-missing-character-mail --log
-${DIR}/console auto-whitelist 1 --log
-${DIR}/console clean-http-cache --log
+"${DIR}"/console update-chars --log --hide-details
+"${DIR}"/console update-corporations --log --hide-details
+"${DIR}"/console update-player-groups --log --hide-details
+"${DIR}"/console check-tokens --log --hide-details
+"${DIR}"/console update-member-tracking --log
+"${DIR}"/console auto-whitelist 1 --log --hide-details
+"${DIR}"/console send-invalid-token-mail --log
+"${DIR}"/console send-missing-character-mail --log
+"${DIR}"/console clean-http-cache --log
