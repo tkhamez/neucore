97b952857c4134945a23afb72bebe5abe67d1a40
Tian Khamez <tkhamez@gmail.com>
2022-05-22 16:03:33 +0200

    Remove update of player name via database migration (was never actually used like this).

--- backend/src/Migrations/Version20190318212603.php
+++ backend/src/Migrations/Version20190318212603.php
@@ -1,16 +1,13 @@
 <?php
+/** @noinspection PhpUnused */
 
 declare(strict_types=1);
 
 namespace Neucore\Migrations;
 
-use Doctrine\DBAL\Exception;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-/**
- * Auto-generated Migration: Please modify to your needs!
- */
 final class Version20190318212603 extends AbstractMigration
 {
     public function getDescription(): string
@@ -18,21 +15,12 @@ final class Version20190318212603 extends AbstractMigration
         return '';
     }
 
-    /**
-     * @throws Exception
-     */
     public function up(Schema $schema): void
     {
-        // this up() migration is auto-generated, please modify it to your needs
-
-        $this->addSql("UPDATE players SET name = CONCAT(name, '#', id) WHERE name NOT LIKE '%#%'");
+        #$this->addSql("UPDATE players SET name = CONCAT(name, '#', id) WHERE name NOT LIKE '%#%'");
     }
 
-    /**
-     * @throws Exception
-     */
     public function down(Schema $schema): void
     {
-        // this down() migration is auto-generated, please modify it to your needs
     }
 }
