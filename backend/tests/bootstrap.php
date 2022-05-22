49f0eab08b0a628aba81448d7b516e1aa0937c14
Tian Khamez <tkhamez@gmail.com>
2022-05-22 22:27:19 +0200

    Use Monolog ErrorHandler for all tests.

--- backend/tests/bootstrap.php
+++ backend/tests/bootstrap.php
@@ -4,14 +4,23 @@ declare(strict_types=1);
 
 namespace Tests;
 
+use Monolog\ErrorHandler;
+use Monolog\Handler\StreamHandler;
+use Monolog\Logger;
 use Neucore\Application;
 
 date_default_timezone_set('UTC');
 
 require __DIR__ . '/../vendor/autoload.php';
 
-// Only used to get the PHP error reporting level from the .env file if it exists
+// Setup error handler
 $config = (new Application())->loadSettings(true);
 error_reporting((int)$config['error_reporting']);
+$handler = new StreamHandler($config['monolog']['path'], Logger::DEBUG);
+$log = new \Neucore\Log\Logger('Test');
+$log->pushHandler($handler);
+ErrorHandler::register($log);
+ini_set('log_errors', '0');
 
+// Create DB schema
 (new Helper())->updateDbSchema();
