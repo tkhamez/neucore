bd443aa9751a97aac970425963602be131c764f6
Tian Khamez <tkhamez@gmail.com>
2022-05-29 16:06:13 +0200

    Update tkhamez/swagger-eve-php, remove exceptions regarding deprecations from the log.

--- backend/src/Log/Logger.php
+++ backend/src/Log/Logger.php
@@ -8,26 +8,6 @@ class Logger extends \Monolog\Logger
 {
     public function addRecord(int $level, string $message, array $context = []): bool
     {
-        // TODO remove once these are fixed
-        if (
-            (
-                (
-                    strpos($message, 'Kevinrob\GuzzleCache') !== false ||
-                    strpos($message, 'League\OAuth2\Client') !== false ||
-                    strpos($message, 'Swagger\Client\Eve') !== false
-                )
-                &&
-                strpos($message, '#[\ReturnTypeWillChange]') !== false
-            )
-
-            ||
-
-            // league/oauth2-client, Tool/QueryBuilderTrait
-            strpos($message, 'E_DEPRECATED: http_build_query(): Passing null to parameter') !== false
-        ) {
-            return true; // do not log
-        }
-
         return parent::addRecord($level, $message, $context);
     }
 }
