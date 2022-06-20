e29ee69acd24b8116fad187668f476584622aa38
Tian Khamez <tkhamez@gmail.com>
2022-06-20 18:07:13 +0200

    Update backend dependencies.

--- backend/src/Log/Logger.php
+++ backend/src/Log/Logger.php
@@ -4,10 +4,16 @@ declare(strict_types=1);
 
 namespace Neucore\Log;
 
+use Monolog\DateTimeImmutable;
+
 class Logger extends \Monolog\Logger
 {
-    public function addRecord(int $level, string $message, array $context = []): bool
-    {
-        return parent::addRecord($level, $message, $context);
+    public function addRecord(
+        int $level,
+        string $message,
+        array $context = [],
+        DateTimeImmutable $datetime = null
+    ): bool {
+        return parent::addRecord($level, $message, $context, $datetime);
     }
 }
