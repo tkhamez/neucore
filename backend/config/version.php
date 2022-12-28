510ba1b1c852b6a6c1530c50d558db72077b03f3
Tian Khamez <tkhamez@gmail.com>
2022-12-28 14:59:55 +0100

    Version 1.40.0

--- backend/config/version.php
+++ backend/config/version.php
@@ -1,3 +1,5 @@
 <?php
 
-const NEUCORE_VERSION = '1.40.0-dev';
+// Note: rebuild the fronted when changed because the version is included in the generated JS API client.
+
+const NEUCORE_VERSION = '1.40.0';
