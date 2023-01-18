241821a05adc178045555c56e0b3d8445e9f63c8
Tian Khamez <tkhamez@gmail.com>
2023-01-18 21:06:42 +0100

    v1.43.0-dev

--- backend/config/version.php
+++ backend/config/version.php
@@ -2,4 +2,4 @@
 
 // Note: rebuild the fronted when changed because the version is included in the generated JS API client.
 
-const NEUCORE_VERSION = '1.42.1';
+const NEUCORE_VERSION = '1.43.0-dev';
