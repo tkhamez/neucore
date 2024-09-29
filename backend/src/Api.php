691c972e1b4d6659a40e7da59759193453dac6a1
Tian Khamez <tkhamez@gmail.com>
2024-09-29 14:01:48 +0200

    swagger-php: Move from annotations to attributes.

--- backend/src/Api.php
+++ backend/src/Api.php
@@ -4,21 +4,10 @@ declare(strict_types=1);
 
 namespace Neucore;
 
-/* @phan-suppress-next-line PhanUnreferencedUseNormal */
-use OpenApi\Annotations as OA;
+use OpenApi\Attributes as OA;
 
-/**
- * @OA\OpenApi(
- *     @OA\Info(
- *         title="Neucore API",
- *         description="Client library of Neucore API",
- *         version=NEUCORE_VERSION
- *     ),
- *     @OA\Server(
- *         url="https://localhost/api"
- *     )
- * )
- */
+#[OA\Info(version: NEUCORE_VERSION, description: "Client library of Neucore API", title: "Neucore API")]
+#[OA\Server(url: "https://localhost/api")]
 class Api
 {
     public const DATE_FORMAT = 'Y-m-d\TH:i:s\Z';
