cbdb4165bcff16225bb58f7e33b04311fdb47418
Tian Khamez <tkhamez@gmail.com>
2025-05-10 18:07:25 +0200

    Version 2.8.0-dev

--- backend/config/version.php
+++ backend/config/version.php
@@ -3,4 +3,4 @@
 // Note: Reinstall the frontend (starting with "composer openapi") after the version was changed
 // because the version is included in the generated JS API client.
 
-const NEUCORE_VERSION = '2.7.3';
+const NEUCORE_VERSION = '2.8.0-dev';
