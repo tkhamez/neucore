84b5cbd722fa6ea702d5583eeca0512d0e1d4772
Tian Khamez <tkhamez@gmail.com>
2025-07-26 15:13:51 +0200

    Replace tkhamez/swagger-eve-php with tkhamez/eve-api-php

--- backend/tests/Unit/Factory/EsiApiFactoryTest.php
+++ backend/tests/Unit/Factory/EsiApiFactoryTest.php
@@ -9,11 +9,11 @@ use Neucore\Service\Config;
 use PHPUnit\Framework\TestCase;
 use Swagger\Client\Eve\Api\AllianceApi;
 use Swagger\Client\Eve\Api\CharacterApi;
-use Swagger\Client\Eve\Api\CorporationApi;
 use Swagger\Client\Eve\Api\MailApi;
 use Swagger\Client\Eve\Api\UniverseApi;
 use Tests\Client;
 use Tests\HttpClientFactory;
+use Tkhamez\Eve\API\Api\CorporationApi;
 
 class EsiApiFactoryTest extends TestCase
 {
@@ -27,20 +27,22 @@ class EsiApiFactoryTest extends TestCase
         ]]);
     }
 
-    public function testGetAllianceApi()
+    public function testGetAllianceApi(): void
     {
         $factory = new EsiApiFactory(new HttpClientFactory(new Client()), $this->config);
         $api = $factory->getAllianceApi();
+        /** @noinspection PhpConditionAlreadyCheckedInspection */
         $this->assertInstanceOf(AllianceApi::class, $api);
     }
 
-    public function testGetCorporationApi()
+    public function testGetCorporationApi(): void
     {
         $factory = new EsiApiFactory(new HttpClientFactory(new Client()), $this->config);
         $api1 = $factory->getCorporationApi();
         $api2 = $factory->getCorporationApi();
         $api3 = $factory->getCorporationApi('access-token');
 
+        /** @noinspection PhpConditionAlreadyCheckedInspection */
         $this->assertInstanceOf(CorporationApi::class, $api1);
         $this->assertSame($api1, $api2);
         $this->assertNotSame($api1, $api3);
@@ -48,26 +50,30 @@ class EsiApiFactoryTest extends TestCase
         $this->assertNotSame($api1->getConfig(), $api3->getConfig());
     }
 
-    public function testGetCharacterApi()
+    public function testGetCharacterApi(): void
     {
         $factory = new EsiApiFactory(new HttpClientFactory(new Client()), $this->config);
         $api1 = $factory->getCharacterApi();
         $api2 = $factory->getCharacterApi('access-token');
 
+        /** @noinspection PhpConditionAlreadyCheckedInspection */
         $this->assertInstanceOf(CharacterApi::class, $api1);
         $this->assertNotSame($api1, $api2);
         $this->assertNotSame($api1->getConfig(), $api2->getConfig());
     }
 
-    public function testGetMailApi()
+    public function testGetMailApi(): void
     {
         $factory = new EsiApiFactory(new HttpClientFactory(new Client()), $this->config);
         $api1 = $factory->getMailApi('token');
         $api2 = $factory->getMailApi('token');
         $api3 = $factory->getMailApi('token2');
 
+        /** @noinspection PhpConditionAlreadyCheckedInspection */
         $this->assertInstanceOf(MailApi::class, $api1);
+        /** @noinspection PhpConditionAlreadyCheckedInspection */
         $this->assertInstanceOf(MailApi::class, $api2);
+        /** @noinspection PhpConditionAlreadyCheckedInspection */
         $this->assertInstanceOf(MailApi::class, $api3);
 
         $this->assertSame($api1, $api2);
@@ -77,10 +83,11 @@ class EsiApiFactoryTest extends TestCase
         $this->assertNotSame($api1->getConfig(), $api3->getConfig());
     }
 
-    public function testGetUniverseApi()
+    public function testGetUniverseApi(): void
     {
         $factory = new EsiApiFactory(new HttpClientFactory(new Client()), $this->config);
         $api = $factory->getUniverseApi();
+        /** @noinspection PhpConditionAlreadyCheckedInspection */
         $this->assertInstanceOf(UniverseApi::class, $api);
     }
 }
