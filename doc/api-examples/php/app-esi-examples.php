2b0b597a041dc9d927492b5ceb5cfd8bbcc8c3ce
Tian Khamez <tkhamez@gmail.com>
2025-08-02 15:54:34 +0200

    Update PHP API example.

--- doc/api-examples/php/app-esi-examples.php
+++ doc/api-examples/php/app-esi-examples.php
@@ -2,11 +2,6 @@
 
 declare(strict_types=1);
 
-use GuzzleHttp\Client;
-use Swagger\Client\Eve\Api\AssetsApi;
-use Swagger\Client\Eve\ApiException;
-use Swagger\Client\Eve\Configuration;
-
 error_reporting(E_ALL & ~E_DEPRECATED);
 
 include __DIR__ . '/vendor/autoload.php';
@@ -15,21 +10,21 @@ include __DIR__ . '/vendor/autoload.php';
  * The endpoint /api/app/v2/esi can be used for any protected ESI GET or POST request.
  *
  * There are two ways to call it, the following examples are for the ESI path and query string:
- * /v5/characters/96061222/assets/?page=1
+ * /characters/96061222/assets/?page=1
  *
  * 1. Compatible with generated OpenAPI clients from the ESI definition file (see example below),
- *    simply append the ESI path to the Neucore path and add the ESI query parameters:
- *    https://neucore.tld/api/app/v2/esi/v5/characters/96061222/assets/?page=1&datasource=96061222
+ *    append the ESI path to the Neucore path and add the ESI query parameters:
+ *    https://neucore.tld/api/app/v2/esi/characters/96061222/assets/?page=1
  *
- * 2. Compatible with generated OpenAPI clients from the Neucore interface definition file (see example below),
- *    the query parameter "esi-path-query" contains the url-encoded ESI path and query string:
- *    https://neucore.tld/api/app/v2/esi?esi-path-query=%2Fv5%2Fcharacters%2F96061222%2Fassets%2F%3Fpage%3D1&datasource=96061222
+ * 2. Compatible with generated OpenAPI clients from the Neucore API definition file (see
+ *    example below). The query parameter "esi-path-query" contains the url-encoded ESI path and query
+ *    string:
+ *    https://neucore.tld/api/app/v2/esi?esi-path-query=%2Fcharacters%2F96061222%2Fassets%2F%3Fpage%3D1
  *
- * Both can use the "datasource" parameter to tell Neucore from which character and EVE login the ESI token
- * should be used for the request. The ESI datasource (tranquility) is decided by the Neucore configuration.
- * Alternatively, the EVE character can be defined with the HTTP header Neucore-EveCharacter and the EVE login
- * from which the token is to be used with Neucore-EveLogin.
- * The header has priority over the query parameter!
+ * The EVE character must be defined with the HTTP header "Neucore-EveCharacter". The EVE login
+ * from which the token is to be used can be changed from "core.default" with the "Neucore-EveLogin"
+ * header. Alternatively, the "datasource" parameter can be used instead of the headers, but the headers
+ * have priority over it.
  *
  * See doc/Documentation.md -> "Authentication of applications" for details about the token.
  */
@@ -42,40 +37,46 @@ include __DIR__ . '/vendor/autoload.php';
 $coreHttpScheme = 'http';
 $coreDomain = 'neucore_http'; // Works with compose.yaml.
 $coreAppToken = base64_encode('1:secret');
-$coreCharId = '96061222'; // Character with token in Neucore
+$coreCharId = 96061222; // Character with token in Neucore
+$compatibilityDate = new DateTime('2025-08-02');
 
 
 //
-// Example 1: A GET request using a generated OpenAPI client from the ESI API file
-//            (e.g. https://packagist.org/packages/tkhamez/swagger-eve-php)
+// Example 1: A GET request using a generated OpenAPI client from the ESI API file,
+//            https://packagist.org/packages/tkhamez/eve-api.
 //
+
+echo PHP_EOL, 'Example 1:', PHP_EOL, PHP_EOL;
 
 // Change the host to the Neucore domain including the API path and add the app token.
-$configuration = new Configuration();
+$configuration = new \Tkhamez\Eve\API\Configuration();
 $configuration->setHost("$coreHttpScheme://$coreDomain/api/app/v2/esi");
 $configuration->setAccessToken($coreAppToken);
 
 // Set default headers to define the EVE character and login for the token to be used.
-$client = new Client(['headers' => [
-    'Neucore-EveCharacter' => $coreCharId,
-    'Neucore-EveLogin' => 'core.default', // This header is optional, core.default is the default value for it.
+$client = new \GuzzleHttp\Client(['headers' => [
+    'Neucore-EveCharacter' => "$coreCharId",
+    'Neucore-EveLogin' => 'core.default', // Optional, "core.default" is the default.
 ]]);
 
-$assetsApiInstance = new AssetsApi($client, $configuration);
+$assetsApiInstance = new \Tkhamez\Eve\API\Api\AssetsApi($client, $configuration);
 $itemId = 0; // used in example 2
 try {
-    $result = $assetsApiInstance->getCharactersCharacterIdAssets($coreCharId);
+    $result = $assetsApiInstance->getCharactersCharacterIdAssets($coreCharId, $compatibilityDate);
     $itemId = $result[0]->getItemId();
     echo 'item id: ', $itemId, PHP_EOL;
-} catch (ApiException $e) {
+} catch (\Tkhamez\Eve\API\ApiException $e) {
     echo $e->getMessage(), PHP_EOL;
 }
 echo PHP_EOL;
 
 
 //
-// Example 2: A POST request using a generated OpenAPI client from the Neucore API file
-//            (e.g. https://packagist.org/packages/bravecollective/neucore-api)
+// Example 2: A POST request using a generated OpenAPI client from the Neucore API file,
+//            https://packagist.org/packages/bravecollective/neucore-api.
+//
+
+echo PHP_EOL, 'Example 2:', PHP_EOL, PHP_EOL;
 
 // Change the host to the Neucore domain including the API path and add the app token
 $config = Brave\NeucoreApi\Configuration::getDefaultConfiguration();
@@ -92,23 +93,12 @@ try {
 
     echo 'Status: ', $result[1], PHP_EOL;
     echo 'Headers: ';
-    $header = [];
-    foreach ($result[2] as $name => $value) {
-        $header[$name] = $value[0];
-    }
+    $header = array_map(function ($value) {
+        return $value[0];
+    }, $result[2]);
     print_r($header);
     echo 'name: ', json_decode($result[0], true)[0]['name'], PHP_EOL;
 } catch (Exception $e) {
     echo 'Exception when calling ApplicationApi->esiV2: ', $e->getMessage(), PHP_EOL;
-}
-echo PHP_EOL;
-
-
-// Example 2a: Public ESI routes are not passed through:
-
-try {
-    $esiApiInstance->esiV2('/lastest/alliances/', $coreCharId);
-} catch (\Brave\NeucoreApi\ApiException $e) {
-    echo $e->getMessage(), PHP_EOL;
 }
 echo PHP_EOL;
